7a74d405937894737fa2dbfb6c0dc0f6a7ba6174
Anurag Yadav <anurag_y@cs.iitr.ac.in>
2026-04-09 18:15:28 +0530

    Add more robust latest checkpoint loading

--- src/training/load_checkpoint.py
+++ src/training/load_checkpoint.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 def load_checkpoint_from_disk(checkpoint_path, trainer, logger):
 	if any(checkpoint_path.iterdir()):
 		choice = input("Do you want to resume training from the checkpoint (Y/N): ").upper()
@@ -6,9 +7,19 @@ def load_checkpoint_from_disk(checkpoint_path, trainer, logger):
 			choice = input("Do you want to resume training from the checkpoint (Y/N): ").upper()
 
 		if choice == "Y":
-			checkpoint = [checkpoint_path for checkpoint_path in checkpoint_path.iterdir()][-1]
-			trainer.train(resume_from_checkpoint=checkpoint)
+			checkpoints = [checkpoint_path for checkpoint_path in checkpoint_path.iterdir()]
+			latest_checkpoint_val = 0
+			checkpoint_idx = 0
+			for idx, checkpoint_path in enumerate(checkpoints):
+				current_checkpoin_val = int(checkpoint_path.stem.split("-")[-1])
+				if (current_checkpoin_val >= latest_checkpoint_val):
+					latest_checkpoint_val = current_checkpoin_val
+					checkpoint_idx = idx
+
+			checkpoint_path = checkpoints[checkpoint_idx]
 			logger.info("Resuming from checkpoint...")
+			trainer.train(resume_from_checkpoint=checkpoint_path)
+
 		else:
 			trainer.train()
 
